d159330e48aea102a4149de78ef24bb33bc01783
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-05-29 00:14:48 +0300

    add Splash screen background image

--- app/src/main/java/com/android/mantrana/SplashScreen.kt
+++ app/src/main/java/com/android/mantrana/SplashScreen.kt
@@ -3,12 +3,16 @@ package com.android.mantrana
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.android.mantrana.ui.theme.MantranaTheme
 
@@ -17,12 +21,15 @@ class SplashScreen : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MantranaTheme {
+                val painter =
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+
                 ) {
-                    Greeting2("Android")
+                    SplashScreenPreview(
+
+                    )
                 }
             }
         }
@@ -30,14 +37,19 @@ class SplashScreen : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String) {
-    Text(text = "Hello $name!")
+fun SplashScreenPreview(
+    modifier: Modifier = Modifier.fillMaxSize(),
+) {
+    Image(painter = painterResource(id = R.drawable.background),
+        contentDescription = "splashscreen Image",
+    contentScale = ContentScale.Fit)
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview2() {
     MantranaTheme {
-        Greeting2("Android")
+        SplashScreenPreview()
     }
 }
