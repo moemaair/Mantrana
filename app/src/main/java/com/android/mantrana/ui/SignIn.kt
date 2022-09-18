a4ee7a459e292d075af371cb6827998d4556ec68
Mohamed Ibrahim <MohamedIbrahim@Mohameds-MacBook-Air-2.local>
2022-09-18 14:50:37 +0300

    add Google service package to Android sdk - Firebase configuration #1

--- app/src/main/java/com/android/mantrana/ui/SignIn.kt
+++ app/src/main/java/com/android/mantrana/ui/SignIn.kt
@@ -1,12 +1,40 @@
 package com.android.mantrana.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.android.mantrana.R
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlin.math.log
 
 class SignIn : AppCompatActivity() {
+
+    private val TAG: String? = SignIn::class.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+       loginBtn.setOnClickListener {
+           checkCredencialCouroutine()
+       }
+
     }
+
+    fun checkCredencialCouroutine(){
+        Log.i(TAG, "start")
+        GlobalScope.launch(Dispatchers.IO) {
+            co()
+        }
+        Log.i(TAG, "end")
+    }
+    suspend fun co() {
+        val email = emailEt.text.toString() // getting inputed email by user
+        val pass = passwardEt.text.toString() // getting inputed password by user
+        Log.i(TAG, "Email: $email \n Password: $pass" )
+    }
+
+
 }
