763c91ab9561c247dbcb466d11c879d653d481c5
Mohamed Ibrahim <MohamedIbrahim@Mohameds-MacBook-Air-2.local>
2022-09-19 16:28:59 +0300

    codes for registering a new user

--- app/src/main/java/com/android/mantrana/ui/SignIn.kt
+++ app/src/main/java/com/android/mantrana/ui/SignIn.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.mantrana.MainActivity
 import com.android.mantrana.R
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -24,16 +25,23 @@ class SignIn : AppCompatActivity() {
 
      //lateinit var email: String
     // lateinit var password: String
-     private lateinit var auth: FirebaseAuth
+     //private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-        auth = Firebase.auth
+        //auth = Firebase.auth
 
         val emailEt = findViewById<EditText>(R.id.emailEt)
 
         val passEt = findViewById<EditText>(R.id.passwardEt)
+
+        //incase not having a an account with mantrana
+
+        SignUpNow.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
 
 //        email = emailEt.text.toString()
 //        password = passEt.text.toString()
@@ -48,24 +56,26 @@ class SignIn : AppCompatActivity() {
            }
            else{
                // [START sign_in_with_email]
-               FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEt.text.toString().trim(), passwardEt.text.toString().trim())
-                   .addOnCompleteListener(this) { task ->
-                       if (task.isSuccessful) {
-                           // Sign in success, update UI with the signed-in user's information
-                           Log.d(TAG, "signInWithEmail:success")
-                           val user = auth.currentUser
-                           updateUI(user)
-                           val intent = Intent(this, MainActivity::class.java)
-                           startActivity(intent)
-                           finish()
-                       } else {
-                           // If sign in fails, display a message to the user.
-                           Log.w(TAG, "signInWithEmail:failure", task.exception)
-                           Toast.makeText(baseContext, "Authentication failed.",
-                               Toast.LENGTH_SHORT).show()
-                           updateUI(null)
-                       }
-                   }
+//               FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEt.text.toString().trim(), passwardEt.text.toString().trim())
+//                   .addOnCompleteListener(this) { task ->
+//                       if (task.isSuccessful) {
+//                           // Sign in success, update UI with the signed-in user's information
+//                           Log.d(TAG, "signInWithEmail:success" + FirebaseAuth.getInstance().currentUser?.uid)
+//                           //FirebaseAuth.getInstance().signOut()
+//                           //val user = auth.currentUser
+//                           updateUI(user)
+//
+////                           val intent = Intent(this, MainActivity::class.java)
+////                           startActivity(intent)
+////                           finish()
+//                       } else {
+//                           // If sign in fails, display a message to the user.
+//                           Log.w(TAG, "signInWithEmail:failure", task.exception)
+//                           Toast.makeText(baseContext, "Authentication failed.",
+//                               Toast.LENGTH_SHORT).show()
+//                           updateUI(null)
+//                       }
+//                   }
                // [END sign_in_with_email]
 
 
@@ -77,17 +87,17 @@ class SignIn : AppCompatActivity() {
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if(currentUser != null){
-            reload();
-        }
+////        val currentUser = auth.currentUser
+//        if(currentUser != null){
+//            reload();
+//        }
     }
 
     private fun updateUI(user: FirebaseUser?) {
 
     }
 }
-private fun reload() {
+fun reload() {
     }
 
 //    fun checkCredencialCouroutine(){
---
+++ app/src/main/java/com/android/mantrana/ui/SignUp.kt
@@ -0,0 +1,115 @@
+package com.android.mantrana.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils.isEmpty
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.android.mantrana.R
+import com.google.android.gms.common.util.CollectionUtils
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUp : AppCompatActivity() {
+    private val TAG: String? = SignUp::class.simpleName
+    private lateinit var name: String
+    private lateinit var email: String
+    private lateinit var password: String
+    private lateinit var cPassword: String
+
+    lateinit var auth: FirebaseAuth
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        //incase user has an account with mantrana
+        signInNow.setOnClickListener {
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+        }
+
+
+        SignUpBtn.setOnClickListener {
+            //check for null valued EditText fields
+            if (!isEmpty(EmailEtSignUp.getText().toString()) && !isEmpty(
+                    PasswordEtSignUp.getText().toString()
+                ) && !isEmpty(PasswordConfirmEtSignUp.getText().toString())
+            ) {
+                //check if passwords match
+                if (doStringMatch(
+                        PasswordEtSignUp.text.toString(),
+                        PasswordConfirmEtSignUp.text.toString()
+                    )
+                ) {
+                    registerNewEmail(
+                        EmailEtSignUp.getText().toString(),
+                        PasswordEtSignUp.getText().toString()
+                    )
+                } else {
+                    Toast.makeText(this, "Passwords do not Match", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else {
+                Toast.makeText(this, "You must fill out all the fields", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private fun registerNewEmail(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun doStringMatch(pass: String, confirmPass: String): Boolean {
+
+        return pass.equals(confirmPass)
+
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+}
+
+
+
